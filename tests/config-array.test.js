4f3b9909fbee5273b14157d5bb1898756b8de282
Nicholas C. Zakas <nicholas@nczconsulting.com>
2019-11-25 11:06:48 -0800

    Chore: Add more tests

--- tests/config-array.test.js
+++ tests/config-array.test.js
@@ -73,9 +73,12 @@ function createConfigArray() {
             files: ["**/*.md", "**/.markdown"],
             language: MarkdownLanguage
         }, {
+            ignores: ["tests/fixtures/**"],
             defs: {
                 name: "config-array"
             }
+        }, {
+            ignores: ["node_modules/**"]
         }, {
             files: ["foo.test.js"],
             defs: {
@@ -101,7 +104,7 @@ function createConfigArray() {
                 }
             }];
         }], {
-        basePath: path.dirname(import.meta.url),
+        basePath,
         schema
     });
 }
@@ -112,80 +115,178 @@ function createConfigArray() {
 
 describe("ConfigArray", () => {
 
-    let configs;
+    let configs,
+        unnormalizedConfigs;
 
-    beforeEach(async () => {
+    beforeEach(() => {
+        unnormalizedConfigs = new ConfigArray([], { basePath });
         configs = createConfigArray();
-        await configs.normalize({
+        return configs.normalize({
             name: "from-context"
         });
     });
-
-    describe("getConfig()", () => {
     
-        it("should calculate correct config when passed JS filename", () => {
-            const filename = path.resolve(basePath, "foo.js");
+    describe("ConfigArray members", () => {
 
-            const config = configs.getConfig(filename);
-            
-            expect(config.language).to.equal(JSLanguage);
-            expect(config.defs).to.be.an("object");
-            expect(config.defs.name).to.equal("config-array");
+        beforeEach(() => {
+            configs = createConfigArray();
+            return configs.normalize({
+                name: "from-context"
+            });
         });
 
-        it("should calculate correct config when passed JS filename that matches two configs", () => {
-            const filename = path.resolve(basePath, "foo.test.js");
-
-            const config = configs.getConfig(filename);
-            
-            expect(config.language).to.equal(JSLanguage);
-            expect(config.defs).to.be.an("object");
-            expect(config.defs.name).to.equal("config-array.test");
+        describe("basePath", () => {
+            it("should store basePath property when basePath is provided", () => {
+                expect(unnormalizedConfigs.basePath).to.equal(basePath);
+                expect(configs.basePath).to.equal(basePath);
+            });
         });
 
-        it("should calculate correct config when passed JS filename that matches a function config", () => {
-            const filename = path.resolve(basePath, "bar.test.js");
+        describe("isNormalized()", () => {
+            it("should return true when the config array is normalized", () => {
+                expect(configs.isNormalized()).to.be.true;
+            });
 
-            const config = configs.getConfig(filename);
-            
-            expect(config.language).to.equal(JSLanguage);
-            expect(config.defs).to.be.an("object");
-            expect(config.defs.name).to.equal("from-context");
+            it("should return false when the config array is not normalized", () => {
+                expect(unnormalizedConfigs.isNormalized()).to.be.false;
+            });
         });
 
-        it("should calculate correct config when passed JS filename that matches a function config returning an array", () => {
-            const filename1 = path.resolve(basePath, "baz.test.js");
-            const config1 = configs.getConfig(filename1);
-            
-            expect(config1.language).to.equal(JSLanguage);
-            expect(config1.defs).to.be.an("object");
-            expect(config1.defs.name).to.equal("baz-from-context");
+        describe("getConfig()", () => {
 
-            const filename2 = path.resolve(basePath, "baz.test.js");
-            const config2 = configs.getConfig(filename2);
-            
-            expect(config2.language).to.equal(JSLanguage);
-            expect(config2.defs).to.be.an("object");
-            expect(config2.defs.name).to.equal("baz-from-context");
+            it("should throw an error when not normalized", () => {
+                const filename = path.resolve(basePath, "foo.js");
+
+                expect(() => {
+                    unnormalizedConfigs.getConfig(filename);
+                }).to.throw(/normalized/);
+            });
+
+            it("should calculate correct config when passed JS filename", () => {
+                const filename = path.resolve(basePath, "foo.js");
+
+                const config = configs.getConfig(filename);
+
+                expect(config.language).to.equal(JSLanguage);
+                expect(config.defs).to.be.an("object");
+                expect(config.defs.name).to.equal("config-array");
+            });
+
+            it("should calculate correct config when passed JS filename that matches two configs", () => {
+                const filename = path.resolve(basePath, "foo.test.js");
+
+                const config = configs.getConfig(filename);
+
+                expect(config.language).to.equal(JSLanguage);
+                expect(config.defs).to.be.an("object");
+                expect(config.defs.name).to.equal("config-array.test");
+            });
+
+            it("should calculate correct config when passed JS filename that matches a function config", () => {
+                const filename = path.resolve(basePath, "bar.test.js");
+
+                const config = configs.getConfig(filename);
+
+                expect(config.language).to.equal(JSLanguage);
+                expect(config.defs).to.be.an("object");
+                expect(config.defs.name).to.equal("from-context");
+            });
+
+            it("should calculate correct config when passed JS filename that matches a function config returning an array", () => {
+                const filename1 = path.resolve(basePath, "baz.test.js");
+                const config1 = configs.getConfig(filename1);
+
+                expect(config1.language).to.equal(JSLanguage);
+                expect(config1.defs).to.be.an("object");
+                expect(config1.defs.name).to.equal("baz-from-context");
+
+                const filename2 = path.resolve(basePath, "baz.test.js");
+                const config2 = configs.getConfig(filename2);
+
+                expect(config2.language).to.equal(JSLanguage);
+                expect(config2.defs).to.be.an("object");
+                expect(config2.defs.name).to.equal("baz-from-context");
+            });
+
+            it("should calculate correct config when passed CSS filename", () => {
+                const filename = path.resolve(basePath, "foo.css");
+
+                const config = configs.getConfig(filename);
+                expect(config.language).to.equal(CSSLanguage);
+                expect(config.defs).to.be.an("object");
+                expect(config.defs.name).to.equal("config-array");
+
+            });
+
+            it("should return the same config when called with the same filename twice", () => {
+                const filename = path.resolve(basePath, "foo.js");
+
+                const config1 = configs.getConfig(filename);
+                const config2 = configs.getConfig(filename);
+
+                expect(config1).to.equal(config2);
+            });
+
         });
 
-        it("should calculate correct config when passed CSS filename", () => {
-            const filename = path.resolve(basePath, "foo.css");
+        describe("files", () => {
 
-            const config = configs.getConfig(filename);
-            expect(config.language).to.equal(CSSLanguage);
-            expect(config.defs).to.be.an("object");
-            expect(config.defs.name).to.equal("config-array");
+            it("should throw an error when not normalized", () => {
+                const filename = path.resolve(basePath, "foo.js");
 
+                expect(() => {
+                    unnormalizedConfigs.files;
+                }).to.throw(/normalized/);
+            });
+
+            it("should return all files from all configs when called", () => {
+                const expectedFiles = configs.reduce((list, config) => {
+                    if (config.files) {
+                        list.push(...config.files);
+                    }
+
+                    return list;
+                }, []);
+                const files = configs.files;
+                expect(files).to.deep.equal(expectedFiles);
+
+            });
         });
 
-        it("should return the same config when called with the same filename twice", () => {
-            const filename = path.resolve(basePath, "foo.js");
+        describe("ignores", () => {
 
-            const config1 = configs.getConfig(filename);
-            const config2 = configs.getConfig(filename);
+            it("should throw an error when not normalized", () => {
+                const filename = path.resolve(basePath, "foo.js");
 
-            expect(config1).to.equal(config2);
+                expect(() => {
+                    unnormalizedConfigs.ignores;
+                }).to.throw(/normalized/);
+            });
+
+            it("should return all ignores from all configs without files when called", () => {
+                const expectedIgnores = configs.reduce((list, config) => {
+                    if (config.ignores && !config.files) {
+                        list.push(...config.ignores);
+                    }
+
+                    return list;
+                }, []);
+                const ignores = configs.ignores;
+                expect(ignores).to.deep.equal(expectedIgnores);
+
+            });
+        });
+
+        describe("push()", () => {
+
+            it("should throw an error when normalized", () => {
+                const filename = path.resolve(basePath, "foo.js");
+
+                expect(() => {
+                    configs.push({});
+                }).to.throw(/extensible/);
+            });
+
         });
 
     });
